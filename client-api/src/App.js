1e685bd46e3f43a0171647edc7ce90849e372b66
piotr kosmala <piotr@wiredrelations.com>
2019-01-27 01:17:05 +0100

    Small update logo

--- client-api/src/App.js
+++ client-api/src/App.js
@@ -2,12 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const AppContext = React.createContext('light');
+const api = '/api/mandelbrot?zoom=%s1&ShiftX=%s2&ShiftY=%s3';
+
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      zoom: 1,
+      shiftX: 0,
+      shiftY:0
+    }
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+          <img src="http://localhost:8081/api/mandelbrot?zoom=2&ShiftX=200&ShiftY=10" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
@@ -17,6 +30,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
+
             Learn React
           </a>
         </header>
